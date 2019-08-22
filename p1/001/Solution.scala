ce77c7c4e00893e74b7345f468be14b73f4e4dbe
哎呀是太阳嘛 <yange1876@hotmail.com>
2019-08-23 02:02:20 +0800

    Update Solution.scala

--- p1/001/Solution.scala
+++ p1/001/Solution.scala
@@ -1,19 +1,16 @@
 import scala.collection.mutable.Map;
-import scala.Mutable.Int
 
 object Solution {
-    def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+     def twoSum(nums: Array[Int], target: Int): Array[Int] = {
         val maps:Map[Int, Int] = Map()
-    
-        val n:Int = 0
-        for (i:Int <- nums) {
-            val s:Int = target - i
-            if (maps.contains(s)) {
-                return Array[Int](maps.get(s).getOrElse(0), n)
-            }
-            maps.put(i, n)
-            n += 1
+        var n:Int = nums.length
+        while (n > 0) {
+          n -= 1
+          val i:Int = nums(n)
+          val t:Int = target - i
+          if (maps.contains(t)) return Array[Int](maps.getOrElse(t, 0) , n)
+          maps += (i -> n)
         }
         Array()
-    }
+      }
 }
