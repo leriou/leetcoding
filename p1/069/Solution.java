04d43db38ac4f5204f0b423a1170449d78f88ad5
哎呀是太阳嘛 <yange1876@hotmail.com>
2019-08-23 15:16:43 +0800

    Update Solution.java

--- p1/069/Solution.java
+++ p1/069/Solution.java
@@ -1,9 +1,9 @@
 class Solution {
+    	private static double delta = 0.1d;
 	public int mySqrt(int x) {
         if (x <= 0) return 0;
         double dx = (double)x;
         double r = dx;
-        double delta = 0.1d;
         while(Math.abs(dx - r * r) > delta) {
             r = (x/r + r) / 2.0d;   
         }
