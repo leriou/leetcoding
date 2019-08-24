c53cb0a33f7b63ef720ab37baeb2ec569f49a4dd
leriou <yange1876@hotmail.com>
2019-08-24 16:40:25 +0800

    add more solutions

--- p2/283/Solution.rs
+++ p2/283/Solution.rs
@@ -1,13 +1,10 @@
-
 impl Solution {
     pub fn move_zeroes(nums: &mut Vec<i32>) {
         let mut pos = 0;
         for i in (0..nums.len()) {
-            println!("{}",i);
-             if (nums[n] != 0) {
-                 let mut tmp = nums[n];
-                 nums.insert(i, nums[pos]);
-                 nums.insert(pos++, tmp);
+             if (nums[i] != 0) {
+                nums.swap(i, pos);
+                pos+=1;
              }
         }     
     }
---
+++ p2/283/Solution.scala
@@ -0,0 +1,15 @@
+object Solution {
+     def moveZeroes(nums: Array[Int]): Unit = {
+        var pos = 0
+        var i = 0
+        while (i < nums.length) {
+          if (nums(i) != 0) {
+            val tmp = nums(i)
+            nums(i) = nums(pos)
+            nums(pos) = tmp
+            pos += 1
+          }
+          i += 1
+        }
+      }
+}
