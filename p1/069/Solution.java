968bde0548b9029c663eddb8e63c840dfe344720
leriou <yange1876@hotmail.com>
2019-08-24 16:47:31 +0800

    format code style

--- p1/069/Solution.java
+++ p1/069/Solution.java
@@ -1,7 +1,7 @@
 class Solution {
-    	private static double delta = 0.1d;
 	public int mySqrt(int x) {
         if (x <= 0) return 0;
+        double delta = 0.1d;
         double dx = (double)x;
         double r = dx;
         while(Math.abs(dx - r * r) > delta) {
