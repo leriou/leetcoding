c0e95db4a290a904a22a4bec79665ed83d8b67e3
leriou <yange1876@hotmail.com>
2019-08-25 12:23:50 +0800

    update code style

--- p7/724/Solution.java
+++ p7/724/Solution.java
@@ -2,12 +2,10 @@ class Solution {
     public int pivotIndex(int[] nums) {
         Integer len = nums.length;
         for (int i = 0; i < len ;i++) {
-            int leftSum = 0;
-            
+            int leftSum = 0; 
             for (int j = 0;j<i;j++) {
                 leftSum += nums[j];
-            }
-            
+            }  
             int rightSum = 0;
             for (int k = (i+1) ; k< len;k++) {
                 rightSum += nums[k];
