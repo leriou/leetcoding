fdfe87cb6e365301803986e04ef94432fe9d5195
leriou <yange1876@hotmail.com>
2019-08-28 20:47:08 +0800

    add 015 solutions in java

--- p1/167/Solution.rs
+++ p1/167/Solution.rs
@@ -1,13 +1,12 @@
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut ri: i32 = numbers.len() as i32 - 1;
-        for (lir, n) in numbers.iter().enumerate() {
-            let li = lir as i32;
+        let mut ri = numbers.len() - 1;
+        for (li, n) in numbers.iter().enumerate() {
             let s = target - n;
             while li < ri {
-                let rn:i32 = *numbers.get(ri as usize).unwrap();
+                let rn:i32 = *numbers.get(ri ).unwrap();
                 if rn == s {
-                    return vec![li + 1, ri +1];
+                    return vec![li as i32 + 1, ri as i32 + 1];
                 } else if rn > s {
                     ri -= 1;
                 } else {
