76799a8718b6035737d6fea4e1fb0cd688cadacb
leriou <yange1876@hotmail.com>
2019-09-08 23:27:59 +0800

    update solution

---
+++ p0/050/Solution.go
@@ -0,0 +1,15 @@
+func myPow(x float64, n int) float64 {
+    if n < 0{
+        x = 1/ x
+        n = -n
+    }
+    res := 1.0
+    for n != 0 {
+        if (n & 1) != 0 {
+            res *= x
+        }
+        x *= x
+        n >>= 1
+    }
+    return res
+}
--- p1/167/Solution.go
+++ p1/167/Solution.go
@@ -1,16 +1,14 @@
 func twoSum(numbers []int, target int) []int {
     ridx := len(numbers) - 1  
-    for idx, n := range numbers {
-        s := target - n
-        for ridx > idx {
-            rn := numbers[ridx]  
-            if s == rn {
-                return []int{idx + 1, ridx + 1} 
-            } else if s < rn {
-                ridx--
-            } else {
-                break;
-            }
+    idx := 0
+    for idx < ridx {
+        s := numbers[ridx] + numbers[idx]
+        if s == target {
+            return []int{idx + 1, ridx + 1}
+        } else if s > target {
+            ridx--
+        } else {
+            idx++
         }
     }
     return []int{}
