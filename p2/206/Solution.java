d65f29b3249dd77f937470b2a32bb9f88103cda2
哎呀是太阳嘛 <yange1876@hotmail.com>
2019-11-19 16:45:27 +0800

    Create Solution.java

---
+++ p2/206/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode newNode = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newNode;
+    }
+}
