fe8ef3a080c4ddd4cd4949f2f582974d9004b298
leriou <yange1876@hotmail.com>
2020-03-22 10:24:34 +0800

    resolve no.365

---
+++ 301_400/365_water_and_jug_problem/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canMeasureWater(int x, int y, int z) {
+        if (x + y < z) return false;
+        if (x == 0 || y == 0) return z == 0 || x + y == z;
+        return z % gcd(x,y) == 0; 
+    }
+    public int gcd(int a, int b){
+        return a == 0 ? b: gcd(b % a, a); 
+    }
+}
