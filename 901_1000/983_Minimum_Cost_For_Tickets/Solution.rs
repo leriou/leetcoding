8f8521000baa0c9dfefed239afa046c57e89cb9d
leriou <yange1876@hotmail.com>
2020-05-08 23:49:28 +0800

    solve 983

--- 901_1000/983_Minimum_Cost_For_Tickets/Solution.rs
+++ 901_1000/983_Minimum_Cost_For_Tickets/Solution.rs
@@ -1,11 +1,7 @@
 impl Solution {
     pub fn mincost_tickets(days: Vec<i32>, costs: Vec<i32>) -> i32 {
-        let len = days.len();
-        let max = days[len - 1] as usize;
-        let min = days[0] as usize;
-        let mut dp = vec![0 as i32; max + 31];
-        let mut d = max;
-        let mut i = len - 1;
+        let (len, max, min) = (days.len(), days[days.len() - 1] as usize, days[0] as usize);
+        let (mut dp, mut d,mut i) = (vec![0 as i32; max + 31], max, len - 1);
         while d >= min {
             if d == days[i] as usize {
                 dp[d] = (dp[d + 1] + costs[0]).min(dp[d + 7] + costs[1]).min(dp[d + 30] + costs[2]);
