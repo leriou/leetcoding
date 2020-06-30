ea93be07f715d66eae9d2eb0264f3d08ee83e912
哎呀是太阳嘛 <yange1876@hotmail.com>
2020-06-30 13:11:13 +0800

    Create Solution.rs

---
+++ 301_400/386_Lexicographical_Numbers/Solution.rs
@@ -0,0 +1,12 @@
+use std::collections::BTreeSet;
+
+
+impl Solution {
+    pub fn lexical_order(n: i32) -> Vec<i32> {
+        let mut set = BTreeSet::new();
+        for i in 1..=n {
+            set.insert(i.to_string());
+        }
+        set.iter().map(|x| x.parse::<i32>().unwrap()).collect()
+    }
+}
