765053682bceafe16a47fbaafc5fb1725694b8e5
哎呀是太阳嘛 <yange1876@hotmail.com>
2020-07-01 13:33:22 +0800

    Update Solution.rs

--- 0_100/011_container_with_most_water/Solution.rs
+++ 0_100/011_container_with_most_water/Solution.rs
@@ -1,23 +1,21 @@
-use std::cmp;
-
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        if  height.len() == 0 {
+        if height.len() == 0 {
             return 0;
         }
-        let mut water: i32   = 0;
-        let mut l            = 0;
-        let mut r            = height.len() -1;
+        let mut water: i32 = 0;
+        let mut l = 0;
+        let mut r = height.len() - 1;
         let mut min_bar: i32 = 0;
-        
+
         while l < r {
-            min_bar = cmp::min(height[l], height[r]);
-            water = cmp::max(water, (r-l) as i32 * min_bar);
+            min_bar = height[l].min(height[r]);
+            water = water.max((r - l) as i32 * min_bar);
             while min_bar >= height[l] && l < r {
-                l+=1;
+                l += 1;
             }
             while min_bar >= height[r] && l < r {
-                r-=1
+                r -= 1
             }
         }
         return water;
