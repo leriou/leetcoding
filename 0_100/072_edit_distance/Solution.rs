b699f9a2830fa06876af2c16210ab027dc110f6b
哎呀是太阳嘛 <yange1876@hotmail.com>
2020-07-01 13:26:01 +0800

    Update Solution.rs

--- 0_100/072_edit_distance/Solution.rs
+++ 0_100/072_edit_distance/Solution.rs
@@ -1,29 +1,28 @@
-use std::cmp::min;
-
 impl Solution {
     pub fn min_distance(word1: String, word2: String) -> i32 {
-        let (m, n) = (word1.len(), word2.len()); 
+        let (m, n) = (word1.len(), word2.len());
         if m == 0 || n == 0 {
             return (m + n) as i32;
         }
-        let mut dp: Vec<Vec<i32>> = vec![vec![0 as i32;n + 1];m + 1];
-        for i in 1..=m {
-            dp[i][0] = i as i32;
-        }
-        for j in 0..=n {
-            dp[0][j] = j as i32;
-        }
-        for i in 1..= m {
-            for j in 1..= n {
-                let l = dp[i-1][j] + 1;
-                let r = dp[i][j-1] + 1;
-                let mut lr = dp[i-1][j-1];
-                if word1.into_bytes()[i-1] != word2.into_bytes()[j-1] {
-                    lr += 1;
+        let (wb1, wb2) = (word1.as_bytes(), word2.as_bytes());
+        let mut dp = vec![vec![0; n + 1]; m + 1];
+        for i in 0..=m {
+            for j in 0..=n {
+                dp[i][j] = if i == 0 {
+                    j
+                } else if j == 0 {
+                    i
+                } else {
+                    let l = dp[i - 1][j] + 1;
+                    let r = dp[i][j - 1] + 1;
+                    let mut lr = dp[i - 1][j - 1];
+                    if wb1[i - 1] != wb2[j - 1] {
+                        lr += 1;
+                    }
+                    l.min(r.min(lr))
                 }
-                dp[i][j] = min(l, min(r, lr));
             }
         }
-        dp[m][n] 
+        dp[m][n] as i32
     }
 }
