ab42e7bd4fab5dcbed2d29a57816096f0a66d32b
哎呀是太阳嘛 <yange1876@hotmail.com>
2020-07-01 13:30:42 +0800

    Update Solution.rs

--- 0_100/001_two_sum/Solution.rs
+++ 0_100/001_two_sum/Solution.rs
@@ -2,17 +2,17 @@ use std::collections::HashMap;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        if (nums.len() < 2) {
-            return vec![];
+        let empty_vec = vec![];
+        if nums.len() >= 2 {
+            let mut maps: HashMap<i32, usize> = HashMap::with_capacity(nums.len());
+            for (n, item) in nums.iter().enumerate() {
+                let s:i32 = target - item;
+                if let Some(pos) = maps.get(&s) {
+                    return vec![maps[&s] as i32, n as i32];
+                }
+                maps.insert(*item, n);
+            }   
         }
-        let mut maps: HashMap<i32, usize> = HashMap::with_capacity(nums.len());
-        for (n, item) in nums.iter().enumerate() {
-            let s:i32 = target - item;
-            if let Some(pos) = maps.get(&s) {
-                return vec![maps[&s] as i32, n as i32];
-            }
-            maps.insert(*item, n);
-        }
-        vec![]
+        empty_vec
     }
 }
