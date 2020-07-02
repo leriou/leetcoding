bbf02a09d7f1806ff092da274252dad6d67351ee
leriou <yange1876@hotmail.com>
2020-07-02 21:44:44 +0800

    resolve 206 for py

--- 201_300/206_reverse_linked_list/Solution.rs
+++ 201_300/206_reverse_linked_list/Solution.rs
@@ -1,7 +1,22 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
 impl Solution {
     pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        let mut curr = head;
-        let mut prev = None;
+        let (mut curr, mut prev) = (head, None);
         while let Some(c) = curr {
             prev = Some(
                 Box::new(ListNode {
