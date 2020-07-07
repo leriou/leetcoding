2d2dd140f8ec81ad1bb6da7727c91bfb9a53cccc
哎呀是太阳嘛 <yange1876@hotmail.com>
2020-07-07 18:13:53 +0800

    Update Solution.java

--- 0_100/002_add_two_numbers/Solution.java
+++ 0_100/002_add_two_numbers/Solution.java
@@ -8,45 +8,27 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null && l2 == null) return null;
-        ListNode first = l1;
-        ListNode second = l2;
+        ListNode pre = new ListNode(0);
+        ListNode cur = pre;
         int carry = 0;
-        ListNode head = new ListNode(0);
-        ListNode prev = head;
-        while (first != null && second != null) {
-            int q = (first.val + second.val + carry) / 10;
-            int r = (first.val + second.val + carry) % 10;
-            carry = q;
-            ListNode node = new ListNode(r);
-            prev.next = node;
-            prev = node;
-            first = first.next;
-            second = second.next;
+        while(l1 != null || l2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = x + y + carry;
+            
+            carry = sum / 10;
+            sum = sum % 10;
+            cur.next = new ListNode(sum);
+
+            cur = cur.next;
+            if(l1 != null)
+                l1 = l1.next;
+            if(l2 != null)
+                l2 = l2.next;
         }
-
-        while (first != null) {
-            int q = (first.val + carry) / 10;
-            int r = (first.val + carry) % 10;
-            carry = q;
-            ListNode node = new ListNode(r);
-            prev.next = node;
-            prev = node;
-            first = first.next;
+        if(carry == 1) {
+            cur.next = new ListNode(carry);
         }
-
-        while (second != null) {
-            int q = (second.val + carry) / 10;
-            int r = (second.val + carry) % 10;
-            carry = q;
-            ListNode node = new ListNode(r);
-            prev.next = node;
-            prev = node;
-            second = second.next;
-        }
-
-        if (carry != 0) prev.next = new ListNode(carry);
-
-        return head.next;
+        return pre.next;
     }
 }
