a80fc2aac1da1fd90d4f15f6096c39e404e2730b
哎呀是太阳嘛 <yange1876@hotmail.com>
2020-07-20 17:47:17 +0800

    Create Solution.java

---
+++ 401_499/416_Partition_Equal_Subset_Sum/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int len = nums.length, sum = 0;
+        for (int i: nums) sum += i;
+        if (sum % 2 == 1) return false;
+        sum = sum /2;
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+        for (int i = 0; i < len; ++i) {
+            for (int j = sum; j >= 0; --j) {
+                if (j >= nums[i]) 
+                    dp[j] = dp[j] || dp[j - nums[i]];
+            }
+        }
+        return dp[sum];
+    }
+}
