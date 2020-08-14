a52ceb744411571af70e4400427ad62c06bb63d2
哎呀是太阳嘛 <yange1876@hotmail.com>
2020-08-14 18:31:33 +0800

    Update Solution.java

--- 0_100/020_Valid_Parentheses/Solution.java
+++ 0_100/020_Valid_Parentheses/Solution.java
@@ -1,20 +1,20 @@
 class Solution {
-    public boolean isValid(String s) {
+  public boolean isValid(String s) {
         int length = s.length();
         if (length == 0) return true;
         if (length % 2 == 1) return false;
-        char[] st = new char[length + 1];
-        st[0] = s.charAt(0);
+        Character[] st = new Character[length + 1];
+        Map<Character, Character> m = new HashMap() {{
+            put(')','(');
+            put('}','{');
+            put(']','[');
+        }};
+        st[0] = Character.valueOf(s.charAt(0));
         int pos = 1;
         for (int i = 1; i < length ; i++) {
-            char cur = s.charAt(i);
-            char stCur = st[pos];
-            if (cur == ')') {
-                if (st[pos-1] == '(') --pos; else return false;
-            } else if (cur == '}') {
-                if (st[pos-1] == '{') { --pos; } else { return false;}
-            } else if (cur == ']') {
-                if (st[pos-1] == '[') { --pos;} else { return false;}
+            Character cur = Character.valueOf(s.charAt(i));
+            if (m.containsKey(cur)) {
+                if (m.get(cur).equals(st[pos-1])) --pos; else return false;
             } else {
                 st[pos++] = cur;
             }
@@ -22,3 +22,5 @@ class Solution {
         return pos == 0;
     }
 }
+
+
