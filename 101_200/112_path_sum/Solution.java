e8a1a2378d1eff012f62c2db931b12facca7c81b
哎呀是太阳嘛 <yange1876@hotmail.com>
2020-08-21 11:58:07 +0800

    Update Solution.java

--- 101_200/112_path_sum/Solution.java
+++ 101_200/112_path_sum/Solution.java
@@ -9,11 +9,9 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int sum) {
-      if (root == null) {return false;}
-      
-       if (root.left == null && root.right == null) {
-        return sum == root.val;
-      }
-      return (root.left != null && this.hasPathSum(root.left, sum- root.val)) || (root.right != null && this.hasPathSum(root.right,sum-root.val));
+      if (root == null) return false;
+      if (root.left == null && root.right == null) return sum == root.val;
+      return this.hasPathSum(root.left, sum- root.val) 
+          || this.hasPathSum(root.right,sum-root.val);
     }
 }
