f88178ac20824f839484743b16b79b11e4f834dc
哎呀是太阳嘛 <yange1876@hotmail.com>
2020-08-28 11:14:30 +0800

    Update Solution.py

--- 101_200/169_Majority_Element/Solution.py
+++ 101_200/169_Majority_Element/Solution.py
@@ -1,4 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums) >> 1]
+        c_num, c = 0,1
+        for i in nums:
+            if i == c_num:
+                c += 1
+            else:
+                c -= 1
+                if c == 0:
+                    c_num,c = i,1
+        return c_num
