873a486a919a3913b549442a7f99b6b1fb5b3214
leriou <yange1876@hotmail.com>
2020-09-06 12:58:29 +0800

    solve 1206

--- 1201_1300/1206_Design_SkipList/Solution.java
+++ 1201_1300/1206_Design_SkipList/Solution.java
@@ -1,125 +1,60 @@
 class Skiplist {
-    final double PROMOTE_RATE = 0.5;
-    Node head, tail;
-    int maxLevel;
-
-    class Node {
+    static class Node {
         int val;
-        Node left, right, up, down;
-        Node(int val) {
+        Node right, down;
+        public Node(Node r, Node d, int val) {
+            right = r;
+            down = d;
             this.val = val;
         }
     }
-
-    public Skiplist() {
-        head = new Node(Integer.MIN_VALUE);
-        tail = new Node(Integer.MAX_VALUE);
-        head.right = tail;
-        tail.left = head;
-    }
-
+    Node head = new Node(null, null, 0);
+    Random rand = new Random();
+    Node[] stack = new Node[64];
     public boolean search(int target) {
-        return findNode(target).val == target;
-    }
-
-    Node findNode(int val) {
-        Node n = head;
-        while (true) {
-            while (n.right.val != Integer.MAX_VALUE
-                    && n.right.val <= val) {
-                n = n.right;
+        // 先往右再往下，缩小区间，套路都是这个套路
+        for (Node p = head; p != null; p = p.down) {
+            while (p.right != null && p.right.val < target) {
+                p = p.right;
             }
-            if (n.down == null) {
-                break;
+            if (p.right != null && p.right.val == target) {
+                return true;
             }
-            n = n.down;
         }
-        return n;
-    }
-
-    private void appendNode(Node preNode, Node newNode){
-        newNode.left=preNode;
-        newNode.right=preNode.right;
-        preNode.right.left=newNode;
-        preNode.right=newNode;
+        return false;
     }
 
     public void add(int num) {
-        Node preNode = findNode(num);
-        // if (preNode.val == num) return;
-        Node node = new Node(num);
-        appendNode(preNode, node);
-        int currentLevel = 0;
-        Random random = new Random();
-        while (random.nextDouble() < PROMOTE_RATE) {
-            if (currentLevel == maxLevel) addLevel();
-            while (preNode.up == null) {
-                preNode = preNode.left;
+        int lv = -1;
+        for (Node p = head; p != null; p = p.down) {
+            while (p.right != null && p.right.val < num) {
+                p = p.right;
             }
-            preNode = preNode.up;
-            Node upperNode = new Node(num);
-            appendNode(preNode, upperNode);
-            upperNode.down = node;
-            node.up = upperNode;
-            node = upperNode;
-            currentLevel++;
+            stack[++lv] = p;
+        }
+        boolean insertUp = true;
+        Node downNode = null;
+        while (insertUp && lv >= 0) {
+            Node insert = stack[lv--];
+            insert.right = new Node(insert.right, downNode, num);
+            downNode = insert.right;
+            insertUp = (rand.nextInt() & 1) == 0;
+        }
+        if (insertUp) {
+            head = new Node(new Node(null, downNode, num), head, 0);
         }
     }
-
-    private void addLevel(){
-        maxLevel++;
-        Node p1=new Node(Integer.MIN_VALUE);
-        Node p2=new Node(Integer.MAX_VALUE);
-        p1.right=p2;
-        p2.left=p1;
-        p1.down=head;
-        head.up=p1;
-        p2.down=tail;
-        tail.up=p2;
-        head=p1;
-        tail=p2;
-    }
-
     public boolean erase(int num) {
-        Node removedNode = findNode(num);
-        if(removedNode.val != num){
-            return false;
-        }
-        int currentLevel=0;
-        while (removedNode != null){
-            removedNode.right.left = removedNode.left;
-            removedNode.left.right = removedNode.right;
-            if(currentLevel != 0
-                    && removedNode.left.val == Integer.MIN_VALUE
-                    && removedNode.right.val == Integer.MAX_VALUE){
-                removeLevel(removedNode.left);
-            }else {
-                currentLevel++;
+        boolean exists = false;
+        for (Node p = head; p != null; p = p.down) {
+            while (p.right != null && p.right.val < num) {
+                p = p.right;
             }
-            removedNode = removedNode.up;
+            if (p.right != null && p.right.val <= num) {
+                exists = true;
+                p.right = p.right.right;
+            }
         }
-        return true;
-    }
-
-    private void removeLevel(Node leftNode){
-        Node rightNode = leftNode.right;
-        if(leftNode.up == null){
-            leftNode.down.up = null;
-            rightNode.down.up = null;
-        }else {
-            leftNode.up.down = leftNode.down;
-            leftNode.down.up = leftNode.up;
-            rightNode.up.down = rightNode.down;
-            rightNode.down.up = rightNode.up;
-        }
-        maxLevel--;
+        return exists;
     }
 }
-
-/**
- * Your Skiplist object will be instantiated and called as such:
- * Skiplist obj = new Skiplist();
- * boolean param_1 = obj.search(target);
- * obj.add(num);
- * boolean param_3 = obj.erase(num);
- */
