6690a5f45c00232270c8d19ccbb7135fae07be71
哎呀是太阳嘛 <yange1876@hotmail.com>
2020-10-15 14:05:04 +0800

    Create Solution.py

---
+++ 101_200/116_Populating_Next_Right_Pointers_in_Each_Node/Solution.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':    
+        if not root: return root
+        cur = root
+        while cur:
+            dummy = Node()
+            tail = dummy
+            while cur:
+                if cur.left:
+                    tail.next = cur.left
+                    tail = tail.next
+                if cur.right:
+                    tail.next = cur.right
+                    tail = tail.next
+                cur = cur.next
+            cur = dummy.next
+        return root
